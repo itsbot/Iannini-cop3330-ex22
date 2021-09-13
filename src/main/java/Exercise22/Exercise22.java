f72b616e5e2c3bb02262ce26e5e70a050d950bbd
itsbot <patrickiannini@knights.ucf.edu>
2021-09-12 23:17:36 -0400

    Initial commit

---
+++ src/main/java/Exercise22/Exercise22.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 22 Solution
+ *  Copyright 2021 Patrick Iannini
+ */
+package Exercise22;
+
+public class Exercise22 {
+    public static void main(String[] args) {
+        
+    }
+}
